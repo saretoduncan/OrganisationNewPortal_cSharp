c1ddc79cee1617b64f823143001186e10c7d4fc6
Duncan <duncan.moiyo@gmail.com>
2022-02-14 14:09:56 +0300

    add departmentnew repository interface

--- Repositories/DepartmentNewsRepository/IDepartmentNews_Repository.cs
+++ Repositories/DepartmentNewsRepository/IDepartmentNews_Repository.cs
@@ -2,11 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using organisationnewportal.Models;
 
 namespace organisationnewportal.Repositories.DepartmentNewsRepository
 {
     public interface IDepartmentNews_Repository
     {
-        
+        Task AddDepartmentNews(DepartmentNews departmentNews);
+        Task GetDepartmentNewsById(int id);
+        Task GetAllDepartmentsNewsByDepartmentId(int id);
+        Task<IEnumerable<DepartmentNews>> GetAllDepartmentNews();
+        Task DeleteDepartmentNews(int id);
+        Task updateDepartmentNews(DepartmentNews departmentNews);
+
     }
 }
